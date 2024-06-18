3ab0c9a0ea40fc9714a80bfe289e2c7f9ae9f63e
Zed-bard <zerihunbard@gmail.com>
2024-06-18 06:04:04 -0400

    Normalize all line endings

--- Frontend/src/components/designLayouts/Image.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Image = ({ imgSrc, className }) => {
-  return <img className={className} src={imgSrc} alt={imgSrc} />;
-};
-
-export default Image;
--- Frontend/src/components/home/Products/Heading.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Heading = ({ heading }) => {
-  return <div className="text-3xl font-semibold pb-6">{heading}</div>;
-};
-
-export default Heading;
