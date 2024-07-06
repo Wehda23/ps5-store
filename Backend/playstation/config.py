d959ad9ce5bf5af3a7f3749c995eb506e3887ed8
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-06 14:10:11 +0300

    Added File config.py to be a clear guide to how to integrate the application to different type of databases and handle different Development phases.

--- Backend/playstation/config.py
+++ Backend/playstation/config.py
@@ -1,6 +1,4 @@
-"""
-Module Contains Configuration for testing
-"""
+# playstation/config.py
 
 import os
 
@@ -8,16 +6,37 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard_to_guess_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(os.getcwd(), 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = False
 
     @staticmethod
     def init_app(app):
         pass
 
+class DevelopmentConfig(Config):
+    DEBUG = True
+
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Use in-memory SQLite for testing
 
+class ProductionConfig(Config):
+    @staticmethod
+    def init_app(app):
+        # Production specific initialization
+        pass
+
+class MySQLProductionConfig(ProductionConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('MYSQL_DATABASE_URL') or 'mysql+pymysql://username:password@localhost/dbname'
+
+class PostgreSQLProductionConfig(ProductionConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRESQL_DATABASE_URL') or 'postgresql://username:password@localhost/dbname'
+
 
 config = {
+    'development': DevelopmentConfig,
     'testing': TestingConfig,
+    'production_mysql': MySQLProductionConfig,
+    'production_postgresql': PostgreSQLProductionConfig,
+
+    'default': DevelopmentConfig
 }
