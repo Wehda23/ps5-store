77b58b48e1e6bdb688a7a282730f6665c9510c76
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-07 19:47:48 +0300

    Added a settings.tsx file that contains domain for Status 'testing' or 'production' for ease of use to integrate it to all API calls

--- Frontend/admin/src/App.tsx
+++ Frontend/admin/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DOMAIN from './settings'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test {DOMAIN} HMR
         </p>
       </div>
       <p className="read-the-docs">
