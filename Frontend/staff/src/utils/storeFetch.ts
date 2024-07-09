020b2997b23f09f47e7216c822e42faa5ca5e71b
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-09 14:03:53 +0300

    Created Proxy Fetch Class for Play Store Frontend

--- Frontend/staff/src/utils/storeFetch.ts
+++ Frontend/staff/src/utils/storeFetch.ts
@@ -1,29 +1,60 @@
 // Create a Proxy Pattern Design Class to fetch data from API endpoints
 
+interface Data {
+    [key: string]: any;
+}
 
+interface Request {
+    (url: string, headers: Data, body: Data): Promise<Response>;
+}
+
+interface GetRequest {
+    (url: string, headers: Data): Promise<Response>;
+}
+
+type DeleteRequest = GetRequest
 
 interface IPSFetch {
-    get(): void;
-    post(): void;
-    put(): void;
-    delete(): void;
+    get: GetRequest;
+    post: Request;
+    put: Request;
+    delete: DeleteRequest;
 }
 
-class PSFetch implements IPSFetch{
-
-    get(){
+class PSFetch implements IPSFetch {
+    async get(url: string, headers: Data): Promise<Response> {
         console.log("get method called");
+        return fetch(url, {
+            method: 'GET',
+            headers: headers
+        });
     }
-    post(){
+
+    async post(url: string, headers: Data, body: Data): Promise<Response> {
         console.log("post method called");
+        return fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body)
+        });
     }
-    put(){
+
+    async put(url: string, headers: Data, body: Data): Promise<Response> {
         console.log("put method called");
+        return fetch(url, {
+            method: 'PUT',
+            headers: headers,
+            body: JSON.stringify(body)
+        });
     }
-    delete(){
+
+    async delete(url: string, headers: Data): Promise<Response> {
         console.log("delete method called");
+        return fetch(url, {
+            method: 'DELETE',
+            headers: headers
+        });
     }
 }
-
 
 export default PSFetch;
