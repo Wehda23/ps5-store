66b8ee6215ec9dcd7e48caacef41242fc738268f
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-09 18:09:43 +0300

    TypeScript Typing for Shipping Addresses Response Data in login_admin

---
+++ Frontend/admin_login/src/services/api/ResponseData.ts
@@ -0,0 +1,30 @@
+export interface Data {
+    [key: string]: any;
+}
+
+export interface IToken {
+    access: string;
+    refresh: string;
+}
+
+export interface IShippingAddress extends Data {
+    address: string;
+    city: string;
+    country: string;
+    default: boolean;
+    state: string;
+}
+
+type ShippingAddressID = number;
+
+type ShippingAddresses = ShippingAddressID[] | IShippingAddress | IShippingAddress[];
+
+export interface IUserInformation {
+    first_name: string;
+    last_name: string;
+    email: string;
+    token: IToken;
+    shipping_addressess?: ShippingAddresses;
+}
+
+export type ResponseData = Data[] | Data | string | IUserInformation;
