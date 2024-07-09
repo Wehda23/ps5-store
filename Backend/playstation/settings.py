21fcfdcfccafe1849bf70089f527a1e164ce0ce6
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-09 15:50:49 +0300

    Added Docstrings for Settings.py documentation

--- Backend/playstation/settings.py
+++ Backend/playstation/settings.py
@@ -36,7 +36,12 @@ ALLOW_IMAGE_TYPES: set[str] = {"png", "jpg", "jpeg"}
 
 # Database
 DATABASE: str = os.getenv("DATABASE", "sqlite:///test.db")
-
+"""
+examples
+DATABASE = "sqlite:///test.db"
+DATABASE = "postgresql://user:password@postgresserver/dbname"
+DATABASE = 'mysql+pymysql://username:password@localhost/dbname'
+"""
 # Secret Key
 SECRET_KEY: str = os.getenv("SECRET_KEY", os.urandom(24))
 
@@ -44,6 +49,7 @@ SECRET_KEY: str = os.getenv("SECRET_KEY", os.urandom(24))
 CORS_ALLOWED_ORIGINS: Union[list[str], str] = ["http://localhost:3000", "http://localhost:5173"]
 """
 [
+    "*", # Allow All Domains
     'http://localhost:3000',  # Example for React development
     'https://example.com'     # Example for a production domain
 ]
@@ -57,7 +63,16 @@ JWT_AUTHENTICATIONS: dict = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": "Bearer ",
 }
-
+"""
+Example
+{
+    "SECRET_KEY": SECRET_KEY,
+    "ALGORITHM": "HS256",
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "AUTH_HEADER_TYPES": "Bearer ",
+}
+"""
 
 # logging configuration
 LOGGING_COFIGURATION: dict[str, str] = {
@@ -65,6 +80,14 @@ LOGGING_COFIGURATION: dict[str, str] = {
     "FILE": "app.logs",
     "FORMAT": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 }
+"""
+Logger Configuration Example
+{
+    "NAME": "playstation",
+    "FILE": "app.logs",
+    "FORMAT": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+}
+"""
 
 # Storage
 STORAGE: str = os.getenv("STORAGE", "")
